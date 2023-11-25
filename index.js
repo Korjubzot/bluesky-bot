32a875f9bff48e8b69af1fb2c25bc31e645d7e13
Korjubzot <korjubzot@gmail.com>
2023-11-25 13:44:26 +1000

    Bot can now upload images as well as text

--- index.js
+++ index.js
@@ -2,23 +2,47 @@ require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const { BskyAgent } = require("@atproto/api");
+const { send } = require("process");
 
-function getGreentext(dirPath) {
-  const files = fs.readdirSync(dirPath);
-  const index = Math.floor(Math.random() * files.length);
-  return path.join(dirPath, files[index]);
-}
+async function sendPostWithImage(imagePath, text) {
+  if (!fs.existsSync(imagePath)) {
+    throw new Error("File does not exist! Try again.");
+  }
 
-async function sendPost(text) {
+  const fileExtension = path.extname(imagePath).toLowerCase();
+  if (fileExtension !== ".jpg" && fileExtension !== ".png") {
+    throw new Error("Only jpg and png files are supported at this time.");
+  }
+
   const agent = new BskyAgent({ service: "https://bsky.social" });
   await agent.login({
     identifier: process.env.IDENTIFIER,
     password: process.env.PASSWORD,
   });
-  await agent.uploadBlob({ text });
+
+  const imageBytes = await fs.promises.readFile(imagePath);
+  const encoding = fileExtension === ".jpg" ? "image/jpeg" : "image/png";
+
+  const testUpload = await agent.uploadBlob(imageBytes, { encoding });
+
+  return await agent.post({
+    text: text,
+    embed: {
+      images: [
+        {
+          image: testUpload.data.blob,
+          alt: "Greentext",
+        },
+      ],
+      $type: "app.bsky.embed.images",
+    },
+  });
 }
 
-const randomFile = getGreentext("./img");
-console.log(randomFile);
-
-sendPost("How's this?");
+sendPostWithImage("./img/1303084653119.jpg", "Tester text")
+  .then(() => {
+    console.log("Post sent!");
+  })
+  .catch((error) => {
+    console.log("Post failed:", error.message);
+  });
